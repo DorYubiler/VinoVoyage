65cb99e502381e5eafded48aab48b924eae3487d
shaniriven <shaniriven@gmail.com>
2024-03-26 14:09:25 +0200

    start of products

---
+++ ViewModel/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VinoVoyage.Models;
+
+namespace VinoVoyage.ViewModel
+{
+    public class ProductViewModel
+    {
+        public ProductModel product {  get; set; }
+        public List<ProductModel> products { get; set; }
+    }
+}
